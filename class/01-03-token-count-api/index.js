c6c7b77d211ced79c192b76f0068649df739788f
Harry <osw0124@daum.net>
2022-05-09 18:04:34 +0900

    함수 재사용 가능하게 변경 연습

--- class/01-03-token-count-api/index.js
+++ class/01-03-token-count-api/index.js
@@ -1,23 +1,28 @@
-function createToeknOfPhone(phone, count) {
-  //휴대폰 번호의 자릿수가 맞는지 확인
-  //인증 토큰을 원하는 자릿수 만큼 생성
-  //핸드폰 번호에 토큰을 전송하는 API
-  if (phone.length !== 10 && phone.length !== 11) {
-    console.log("휴대폰 번호의 자릿수가 맞지 않습니다.");
+function createTokenOfPhone(phoneNum) {
+  //1.핸드폰 번호가 10~11자리인지 확인
+  if (phoneNum.length !== 10 && phoneNum.length !== 11) {
+    console.log("에러발생!!! 핸드폰 번호를 제대로 입력해 주세요!!!");
     return;
   }
 
-  if (count == undefined) {
-    console.log("에러발생!!! 갯수를 입력해주세요.");
-  } else if (count <= 0) {
-    console.log("에러발생!!! 갯수가 너무 적습니다.");
-  } else if (count >= 10) {
-    console.log("에러발생!! 갯수가 너무 많습니다.");
-  } else {
-    const token = String(Math.floor(Math.random() * 10 ** count)).padStart(count, "0");
+  //2.토큰 생성(getToken)
+  const count = 5;
 
-    console.log(`${phone}번호로 인증번호 ${token}을 전송합니다.`);
+  if (count === undefined || count === null) {
+    console.log("에러발생!!! 갯수를 제대로 입력해 주세요");
+    return;
+  } else if (count <= 0) {
+    console.log("에러발생!!! 갯수가 너무 적습니다");
+    return;
+  } else if (count > 10) {
+    console.log("에러발생!!! 갯수가 너무 많습니다");
+    return;
   }
+
+  const token = String(Math.floor(Math.random() * 10 ** count)).padStart(count, "0");
+
+  //3.토큰을 핸드폰번호로 전송
+  console.log(phoneNum + "번호로 인증번호" + token + "를 전송합니다!!!");
 }
 
-createToeknOfPhone("01012345678", 6);
+createTokenOfPhone("01012345678");
