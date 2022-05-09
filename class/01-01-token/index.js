2a5488d46ac232ebd911e8e76a35f1edc99404b2
Harry <osw0124@daum.net>
2022-05-09 17:24:05 +0900

    주석 추가 + 리턴 연습

--- class/01-01-token/index.js
+++ class/01-01-token/index.js
@@ -1,8 +1,27 @@
-console.log("안녕하세요~~~");
+//하나의 함수는 하나의 기능만
 
+//parameter->param
 function getToken(aaa) {
   const token = String(Math.floor(Math.random() * 10 ** aaa)).padStart(aaa, "0");
   console.log(token);
 }
 
-getToken(13);
+function add(aaa, bbb) {
+  const result = aaa + bbb;
+  return result;
+}
+
+function minus(aaa, bbb) {
+  const result = aaa - bbb;
+  return result;
+}
+
+//argument->args
+getToken(6);
+
+//return
+const aaa = add(1, 2);
+const bbb = minus(12, 10);
+
+const zzz = aaa * bbb;
+console.log(zzz);
