1774462edc6d517d109c9fc2e60ea9a29c8f4713
Harry <osw0124@daum.net>
2022-05-09 17:38:23 +0900

    예외처리

--- class/01-02-token-count/index.js
+++ class/01-02-token-count/index.js
@@ -1,20 +1,19 @@
-console.log("안녕하세요~~");
-
 function getToken(count) {
-  if (count == undefined) {
-    console.log("에러발생!!! 갯수를 입력해주세요.");
+  if (count === undefined || count === null) {
+    console.log("에러발생!!! 갯수를 제대로 입력해 주세요");
+    return;
   } else if (count <= 0) {
-    console.log("에러발생!!! 갯수가 너무 적습니다.");
-  } else if (count >= 10) {
-    console.log("에러발생!! 갯수가 너무 많습니다.");
-  } else {
-    const result = String(Math.floor(Math.random() * 10 ** count)).padStart(count, "0");
-    console.log(result);
-
-    return result;
+    console.log("에러발생!!! 갯수가 너무 적습니다");
+    return;
+  } else if (count > 10) {
+    console.log("에러발생!!! 갯수가 너무 많습니다");
+    return;
   }
+
+  const token = String(Math.floor(Math.random() * 10 ** count)).padStart(count, "0");
+  console.log(token);
 }
 
-let count = 8;
+let count = 5;
 
-getToken(count);
+getToken(6);
