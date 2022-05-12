55b547f631bc693659a3eb83d482160a196017e0
woong <osw0124@daum.net>
2022-05-12 18:24:07 +0900

    day04-rest api 연습

---
+++ class/04-03-rest-api-with-swagger/server.js
@@ -0,0 +1,45 @@
+// const express = require("express");
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+import { createTokenOfPhone } from "../01-05-token-count-api-facade-import/index.js";
+import { options } from "./swagger/config.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
+
+app.get("/boards", (req, res) => {
+  //1. 데이터를 조회하는 로직 => DB에 접속해서 데이터 꺼내오기
+  const result = [
+    { number: 1, wirter: "철수", title: "제목입니다~", contents: "내용이에요!!" },
+    { number: 2, wirter: "로직", title: "제목입니다~", contents: "내용이에요!!" },
+    { number: 3, wirter: "흰둥이", title: "제목입니다~", contents: "내용이에요!!" },
+  ];
+  //2.꺼내온 결과 응답 주기
+  res.status(200).send(result);
+});
+
+app.post("/boards", (req, res) => {
+  console.log(req.body);
+  //1.데이터를 등록하는 로직 => DB에 접속해서 데이터 저장하기
+  //2.저장 결과 메세지
+  res.status(201).send({ msg: "게시물 등록에 성공하였습니다." });
+});
+
+app.post("/tokens/phone", (req, res) => {
+  const myphone = req.body.myphone;
+  const token = createTokenOfPhone(myphone);
+
+  res.status(200).send({
+    token: token,
+    msg: "토큰을 받아주세요",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
