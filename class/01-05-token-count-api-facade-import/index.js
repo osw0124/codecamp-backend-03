808e28b49e820dfe3e6ede9b0967faf9c89d72b4
woong <osw0124@daum.net>
2022-05-12 17:16:28 +0900

    콘솔 제거, 일부 기능 주석 처리

--- class/01-05-token-count-api-facade-import/index.js
+++ class/01-05-token-count-api-facade-import/index.js
@@ -1,8 +1,6 @@
 import { checkValidationPhone, getToken, sendTokenToSMS } from "./phone.js";
 
-console.log("안녕하세요~~");
-
-function createTokenOfPhone(myphone) {
+export function createTokenOfPhone(myphone) {
   // 1. 휴대폰번호 자릿수 맞는지 확인하기
   const isValid = checkValidationPhone(myphone);
   if (isValid) {
@@ -10,8 +8,8 @@ function createTokenOfPhone(myphone) {
     const mytoken = getToken();
 
     // 3. 핸드폰번호에 토큰 전송하기
-    sendTokenToSMS(myphone, mytoken);
+    // sendTokenToSMS(myphone, mytoken);
+
+    return mytoken;
   }
 }
-
-createTokenOfPhone("01012345678");
