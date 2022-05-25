6d1a6ed6e652ba1d28fed6c4d5f1fb4d8632eb12
woong <osw0124@daum.net>
2022-05-25 18:44:59 +0900

    day13 수업 분 nest, graphql

---
+++ class/13-02-nestjs-with-typeorm/README.md
@@ -0,0 +1,18 @@
+# **graphQL on Nestjs**
+
+## **MySQL**
+
+- ORM 사용(typeorm)
+
+---
+
+1. typeORM 설치 // install Docs참고
+2. app.module 설정 // Docs 참고, 싱크로나이즈 주의!!
+3. 테이블 생성, 연결(export, import)
+4. DBeaver 연결 DB 생성, 테이블 확인
+
+---
+
+### **참고**
+
+- https://docs.nestjs.com/techniques/database
---
+++ class/13-02-nestjs-with-typeorm/src/app.module.ts
@@ -0,0 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardModule } from './apis/boards/boards.module';
+import { Board } from './apis/boards/entities/board.entity';
+
+@Module({
+  imports: [
+    BoardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schma.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12341234',
+      database: 'myproject03',
+      entities: [Board], //collection
+      synchronize: true, //DB와 설정을 동기화 하겠다
+      logging: true,
+    }),
+  ],
+})
+export class AppModule {}
